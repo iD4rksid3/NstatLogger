9b6059dbdfd1fcf1d833226680e84df91c987f80
id4rksid3 <d4rksid3@protonmail.com>
2020-06-15 22:56:21 +0400

    autopep8 applied

--- NstatLogger.py
+++ NstatLogger.py
@@ -17,55 +17,65 @@ from socket import AF_INET, SOCK_STREAM, SOCK_DGRAM
 
 
 class NstatLogger:
-    
-    banner = ''' 
-    _   __     __        __  __                               
+
+    banner = '''
+    _   __     __        __  __
    / | / /____/ /_____ _/ /_/ /   ____  ____ _____ ____  _____
   /  |/ / ___/ __/ __ `/ __/ /   / __ \/ __ `/ __ `/ _ \/ ___/
- / /|  (__  ) /_/ /_/ / /_/ /___/ /_/ / /_/ / /_/ /  __/ /    
-/_/ |_/____/\__/\__,_/\__/_____/\____/\__, /\__, /\___/_/     
-                                     /____//____/ v1.5    ©Mayed.alm    
-                                     
+ / /|  (__  ) /_/ /_/ / /_/ /___/ /_/ / /_/ / /_/ /  __/ /
+/_/ |_/____/\__/\__,_/\__/_____/\____/\__, /\__, /\___/_/
+                                     /____//____/ v1.5    ©Mayed.alm
+
             '''
-    
-    def __init__(self):      
-        self.start = 'Started: ' + time.ctime()[:19]+'\n'  #print the start time
+
+    def __init__(self):
+        self.start = 'Started: ' + \
+            time.ctime()[:19] + '\n'  # print the start time
         if os.name == 'nt':
-            self.tempdir = tempfile.gettempdir()+'\\'
+            self.tempdir = tempfile.gettempdir() + '\\'
         elif os.name == 'posix':
-            self.tempdir = tempfile.gettempdir()+'/'
-        self.filename = 'NstatLogger-'+time.ctime()[11:19].replace(':','-')+'.log' #file name with time 
+            self.tempdir = tempfile.gettempdir() + '/'
+        self.filename = 'NstatLogger-' + \
+            time.ctime()[11:19].replace(':', '-') + '.log'  # file name with time
         self.tempfile = self.tempdir + self.filename
-        self.mutex = threading.Lock() #Mutex to control threads
-        
-    def cmd_args(self):        
+        self.mutex = threading.Lock()  # Mutex to control threads
+
+    def cmd_args(self):
         print(NstatLogger.banner)
-        parser = argparse.ArgumentParser(prog='NstatLogger',\
-            description=' [+] Log netstat like TCP/IP and UDP connections from host',\
+        parser = argparse.ArgumentParser(
+            prog='NstatLogger',
+            description=' [+] Log netstat like TCP/IP and UDP connections from host',
             epilog='[+] Example: NstatLogger -i 2 -t 3600 (will run NstatLogger for one hour, with 2 seconds interval/refresh)')
-        parser.add_argument('-t', '--timer', type=int,default=99**10, help='Set timer in seconds\
+        parser.add_argument(
+            '-t', '--timer', type=int, default=99**10, help='Set timer in seconds\
         of when to stop capturing, default will run until user termination (ctrl+c)')
-        parser.add_argument('-i', '--interval', type=int, default=3, help='Set capturing interval\
+        parser.add_argument(
+            '-i',
+            '--interval',
+            type=int,
+            default=3,
+            help='Set capturing interval\
         in seconds, default is 3')
         self.args = parser.parse_args()
         self.interval = self.args.interval
         self.timer = self.args.timer
         return parser.parse_args()
-    
-    
-    def timer_func(self):  #function to control execution time
-        self.current_time =  0     
+
+    def timer_func(self):  # function to control execution time
+        self.current_time = 0
         self.time_to_stop = self.current_time + self.timer
-        while self.current_time <= self.time_to_stop:   
+        while self.current_time <= self.time_to_stop:
             self.current_time += 1
             time.sleep(1)
-            print('Running (seconds): {:d}\r'.format(self.current_time), end='')
+            print(
+                'Running (seconds): {:d}\r'.format(
+                    self.current_time), end='')
             if self.current_time == self.time_to_stop:
-                self.mutex.release() #releasing the lock for the threading to allow application to continue execution
+                # releasing the lock for the threading to allow application to
+                # continue execution
+                self.mutex.release()
                 break
-                
-    
-    
+
     def capture(self):
         self.AD = '-'
         AF_INET6 = getattr(socket, 'AF_INET6', object())
@@ -74,24 +84,37 @@ class NstatLogger:
             (AF_INET6, SOCK_STREAM): 'tcp6',
             (AF_INET, SOCK_DGRAM): 'udp',
             (AF_INET6, SOCK_DGRAM): 'udp6',
-            }
-        self.templ = '%-5s %-5s %-5s %-5s %-5s %-5s %-5s %s' #String formatting
+        }
+        self.templ = '%-5s %-5s %-5s %-5s %-5s %-5s %-5s %s'  # String formatting
         self.proc_names = {}
         self.proc_cmd = {}
         self.proc_time = {}
-      
-        
+
         def capture_action():
-            print(self.start+'\nPress Ctrl+c to properly stop of the tool!\nDON\'T OPEN THE CSV FILE WHILE THE TOOL IS RUNNING!')
+            print(
+                self.start +
+                '\nPress Ctrl+c to properly stop of the tool!\nDON\'T OPEN THE CSV FILE WHILE THE TOOL IS RUNNING!')
             with open(self.tempfile, 'w') as f:
-                f.write(self.start + '\n'+ self.templ % \
-                    ('Proto|', 'Local address|', 'Remote address|', 'Status|',\
-                     'PID|', 'Program name|', 'Time started|', 'Command line') + '\n')            
+                f.write(
+                    self.start +
+                    '\n' +
+                    self.templ %
+                    ('Proto|',
+                     'Local address|',
+                     'Remote address|',
+                     'Status|',
+                     'PID|',
+                     'Program name|',
+                     'Time started|',
+                     'Command line') +
+                    '\n')
             while self.current_time <= self.time_to_stop:
                 for p in psutil.process_iter(attrs=['pid', 'name', 'cmdline']):
-                    self.proc_names[p.info['pid']] = p.info['name'] #assign key as pid and value as process name
+                    # assign key as pid and value as process name
+                    self.proc_names[p.info['pid']] = p.info['name']
                     self.proc_cmd[p.info['pid']] = p.info['cmdline']
-                    self.proc_time[p.info['pid']] = str(p).split(',')[2][9:].replace(')','')
+                    self.proc_time[p.info['pid']] = str(
+                        p).split(',')[2][9:].replace(')', '')
                 for c in psutil.net_connections(kind='inet'):
                     if c.status == psutil.CONN_ESTABLISHED\
                        or c.status == psutil.CONN_SYN_SENT\
@@ -104,77 +127,82 @@ class NstatLogger:
                        or c.status == psutil.CONN_LAST_ACK\
                        or c.status == psutil.CONN_CLOSING\
                        or c.status == psutil.CONN_NONE:
-                        if c.laddr.ip not in  ['0.0.0.0','::','127.0.0.1', '::1']: #ignor localhost ip addresses
-                            if not c.laddr.ip.startswith('fe80') and not c.laddr.ip.startswith('169.254.'): #ignore local-link address
+                        if c.laddr.ip not in [
+                                '0.0.0.0', '::', '127.0.0.1', '::1']:  # ignor localhost ip addresses
+                            if not c.laddr.ip.startswith('fe80') and not c.laddr.ip.startswith(
+                                    '169.254.'):  # ignore local-link address
                                 laddr = '%s:%s' % (c.laddr)
                                 self.raddr = ''
                                 if c.raddr:
                                     self.raddr = '%s:%s' % (c.raddr)
-                                with open(self.tempfile ,'a+') as f:
-                                        f.writelines((self.templ % \
-                                            (self.proto_map[(c.family, c.type)]+'|', laddr+'|', self.raddr+'|', \
-                                                c.status+'|', str(c.pid)+'|' or self.AD+'|', self.proc_names.get(c.pid, '?')+'|',\
-                                                self.proc_time.get(c.pid, '?')+'|', self.proc_cmd.get(c.pid, '?'))+'|', '\n'))
+                                with open(self.tempfile, 'a+') as f:
+                                    f.writelines((self.templ %
+                                                  (self.proto_map[(c.family, c.type)] +
+                                                   '|', laddr +
+                                                   '|', self.raddr +
+                                                   '|', c.status +
+                                                   '|', str(c.pid) +
+                                                      '|' or self.AD +
+                                                      '|', self.proc_names.get(c.pid, '?') +
+                                                      '|', self.proc_time.get(c.pid, '?') +
+                                                      '|', self.proc_cmd.get(c.pid, '?')) +
+                                                  '|', '\n'))
                                 self.uniq(self.tempfile)
                 time.sleep(self.interval)
                 if self.current_time == self.time_to_stop:
-                    self.mutex.acquire() #locking (waiting) for the other thread to terminate
+                    self.mutex.acquire()  # locking (waiting) for the other thread to terminate
                     break
                 else:
                     continue
-        
-                    
+
         def main_threads():
             if self.interval and self.timer:
-                th1 = threading.Thread(target = self.timer_func)
-                th2 = threading.Thread(target = capture_action)
+                th1 = threading.Thread(target=self.timer_func)
+                th2 = threading.Thread(target=capture_action)
                 th1.daemon = True
                 th2.daemon = True
                 self.mutex.acquire()
-                th1.start() ; th2.start()
+                th1.start()
+                th2.start()
                 while self.current_time <= self.time_to_stop:
                     time.sleep(self.interval)
                     if self.current_time == self.time_to_stop:
-                        th1.join() #wait until t1 terminates (make sure its finished)
-                        th2.join() #wait until t2 terminates (make sure its finished)
+                        th1.join()  # wait until t1 terminates (make sure its finished)
+                        th2.join()  # wait until t2 terminates (make sure its finished)
                         self.end()
-                    
+
         main_threads()
-           
-        
-    def uniq(self, file_to_clean): #write only uniq lines to output file      
-        lines_seen = set() # holds lines already seen
+
+    def uniq(self, file_to_clean):  # write only uniq lines to output file
+        lines_seen = set()  # holds lines already seen
         with open(self.filename, 'w') as outfile:
             for line in open(file_to_clean, 'r'):
-                if line not in lines_seen: # not a duplicate
+                if line not in lines_seen:  # not a duplicate
                     outfile.write(line)
-                    lines_seen.add(line)  
-        with open(self.filename, 'r') as txt_file: #make csv file from the txt file
+                    lines_seen.add(line)
+        with open(self.filename, 'r') as txt_file:  # make csv file from the txt file
             stripped = (line.strip() for line in txt_file)
             lines = (line.split("|") for line in stripped if line)
-            with open(self.filename[:-3]+'csv', 'w') as csv_file:
+            with open(self.filename[:-3] + 'csv', 'w') as csv_file:
                 writer = csv.writer(csv_file)
-                writer.writerows(lines)        
-    
+                writer.writerows(lines)
+
     def end(self):
-        end_txt = ('\n'+'Ended: ' + time.ctime()[:19])      
+        end_txt = ('\n' + 'Ended: ' + time.ctime()[:19])
         with open(self.filename, 'a') as txt_file_end:
             txt_file_end.write(end_txt)
         end_csv = ['Ended: ' + time.ctime()[:19]]
-        with open(self.filename[:-3]+'csv', 'a') as csv_file_end:
+        with open(self.filename[:-3] + 'csv', 'a') as csv_file_end:
             writer = csv.writer(csv_file_end)
-            writer.writerow(end_csv)    
-        os.remove(self.tempfile) #remove the non-uniq log file
+            writer.writerow(end_csv)
+        os.remove(self.tempfile)  # remove the non-uniq log file
         sys.exit(end_txt)
-                
-        
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     try:
         n = NstatLogger()
         n.cmd_args()
         n.capture()
     except KeyboardInterrupt:
         n.end()
-    
-    
--- NstatResolver.py
+++ NstatResolver.py
@@ -16,31 +16,33 @@ import requests
 from socket import AF_INET, SOCK_STREAM, SOCK_DGRAM
 from requests.packages.urllib3.contrib import pyopenssl as reqs
 
+
 class NstatResolver:
 
-    banner = ''' 
-    _   __     __        __  ____                  __               
+    banner = '''
+    _   __     __        __  ____                  __
    / | / /____/ /_____ _/ /_/ __ \___  _________  / /   _____  _____
   /  |/ / ___/ __/ __ `/ __/ /_/ / _ \/ ___/ __ \/ / | / / _ \/ ___/
- / /|  (__  ) /_/ /_/ / /_/ _, _/  __(__  ) /_/ / /| |/ /  __/ /    
-/_/ |_/____/\__/\__,_/\__/_/ |_|\___/____/\____/_/ |___/\___/_/     
-                                                v1.5    ©Mayed.alm    
+ / /|  (__  ) /_/ /_/ / /_/ _, _/  __(__  ) /_/ / /| |/ /  __/ /
+/_/ |_/____/\__/\__,_/\__/_/ |_|\___/____/\____/_/ |___/\___/_/
+                                                v1.5    ©Mayed.alm
 
             '''
 
     def __init__(self):
-        self.vt_api_key = "" #Edit with your virus total API key
+        self.vt_api_key = ""  # Edit with your virus total API key
         print(self.banner)
         if len(sys.argv) < 2:
             ip_input = input("Enter hostname/IP: ")
             self.reslove_func_ip(ip_input)
         elif len(sys.argv) > 2:
-            sys.exit(input('ERROR: Supply one log file at once! Press Enter to exit'))
+            sys.exit(
+                input('ERROR: Supply one log file at once! Press Enter to exit'))
         self.reslove_func(sys.argv[1])
 
+    # func to performe reverse ip lookup and dns history/ ssl alt names
 
-
-    def reslove_func(self,filename): #func to performe reverse ip lookup and dns history/ ssl alt names
+    def reslove_func(self, filename):
         filename_base_name = os.path.basename(filename)
         ip_list = []
         dns_alt_names = []
@@ -48,19 +50,22 @@ class NstatResolver:
         revers_lookup = {}
         notFound = '-'
         try:
-            choice = int(input('Choose option:\n 1) Search for DNS history through Threat Crowd and VirusTotal APIs (recommended).\
+            choice = int(
+                input('Choose option:\n 1) Search for DNS history through Threat Crowd and VirusTotal APIs (recommended).\
             \n 2) Get SSL certificate alternative names (not recommended for suspecious connections as it connects to each IP).\n:'))
         except ValueError:
             sys.exit('Unknown option!')
-        #if not filename.startswith('NstatLogger'):
+        # if not filename.startswith('NstatLogger'):
             #sys.exit('ERROR: Unable to read file, make sure it\'s an NstatLogger log file!')
         with open(filename, 'r') as file_in:
-            with open('Resolved-'+filename_base_name, 'w') as file_out_csv: #csv file out
+            with open('Resolved-' + filename_base_name, 'w') as file_out_csv:  # csv file out
                 writer = csv.writer(file_out_csv)
                 if choice == 1:
-                    writer.writerows([('Program name','Remote address','Domain name','DNS history')]) 
+                    writer.writerows(
+                        [('Program name', 'Remote address', 'Domain name', 'DNS history')])
                 elif choice == 2:
-                    writer.writerows([('Program name', 'Remote address', 'Domain name', 'SSL alternative names')])
+                    writer.writerows(
+                        [('Program name', 'Remote address', 'Domain name', 'SSL alternative names')])
                 else:
                     sys.exit('Unknown option!')
                 print('Resolving...')
@@ -68,41 +73,55 @@ class NstatResolver:
                 for lines in file_in.readlines()[3:-2]:
                     try:
                         ip_addr = lines.split(',')[2]
-                        ip_addr = ip_addr.rsplit(':',1)[0].strip() #get the ip only without the port
-                        program_name = lines.split(',')[5] #get the program name
-                    except:
+                        # get the ip only without the port
+                        ip_addr = ip_addr.rsplit(':', 1)[0].strip()
+                        program_name = lines.split(
+                            ',')[5]  # get the program name
+                    except BaseException:
                         continue
                     if ip_addr in ip_list:
                         continue
-                    if lines.split(',')[2] == ' ': # ignoring program names with no remote ip address (e.g udp)
+                    if lines.split(
+                            ',')[2] == ' ':  # ignoring program names with no remote ip address (e.g udp)
                         continue
                     else:
-                        prog_name_lst.append(program_name) #append program names to the prog_name_lst
-                        ip_list.append(ip_addr) #append ip addresses to the ip_list
-                        line_counter += 1                     
+                        # append program names to the prog_name_lst
+                        prog_name_lst.append(program_name)
+                        # append ip addresses to the ip_list
+                        ip_list.append(ip_addr)
+                        line_counter += 1
 
                 with alive_bar(line_counter) as bar:
-                    for ip, prog in zip( ip_list,prog_name_lst):
+                    for ip, prog in zip(ip_list, prog_name_lst):
                         try:
-                            revers_lookup.update({ip: socket.gethostbyaddr(ip)})                
+                            revers_lookup.update(
+                                {ip: socket.gethostbyaddr(ip)})
                         except (socket.herror, socket.gaierror, OSError):
                             revers_lookup.update({ip: notFound})
-        
+
                         if choice == 1:
-                            threat_crowdAPI = requests.get("http://www.threatcrowd.org/searchApi/v2/ip/report/", {"ip": ip})
+                            threat_crowdAPI = requests.get(
+                                "http://www.threatcrowd.org/searchApi/v2/ip/report/", {"ip": ip})
                             threat_crowdJSON = json.loads(threat_crowdAPI.text)
-                            vt_api = requests.get(f"https://www.virustotal.com/vtapi/v2/ip-address/report?apikey={self.vt_api_key}&ip={ip}")
+                            vt_api = requests.get(
+                                f"https://www.virustotal.com/vtapi/v2/ip-address/report?apikey={self.vt_api_key}&ip={ip}")
                             try:
                                 vt_apiJSON = json.loads(vt_api.text)
-                                writer.writerows([(prog, ip, revers_lookup[ip], vt_apiJSON['resolutions'], threat_crowdJSON['resolutions'][::-1])])
+                                writer.writerows([(prog,
+                                                   ip,
+                                                   revers_lookup[ip],
+                                                   vt_apiJSON['resolutions'],
+                                                   threat_crowdJSON['resolutions'][::-1])])
                                 bar()
-                            except:
+                            except BaseException:
                                 pass
                                 try:
-                                    writer.writerows([(prog, ip, revers_lookup[ip], threat_crowdJSON['resolutions'][::-1])])
+                                    writer.writerows(
+                                        [(prog, ip, revers_lookup[ip], threat_crowdJSON['resolutions'][::-1])])
                                     bar()
                                 except KeyError:
-                                    writer.writerows([(prog, ip, revers_lookup[ip])])
+                                    writer.writerows(
+                                        [(prog, ip, revers_lookup[ip])])
                                     bar()
                                     continue
                         elif choice == 2:
@@ -111,59 +130,71 @@ class NstatResolver:
                                     reqs.OpenSSL.crypto.FILETYPE_PEM,
                                     reqs.ssl.get_server_certificate((ip, 443)))
                                 dns_alt_names = reqs.get_subj_alt_name(x509)
-                                writer.writerows([(prog, ip, revers_lookup[ip], dns_alt_names)])
+                                writer.writerows(
+                                    [(prog, ip, revers_lookup[ip], dns_alt_names)])
                                 bar()
-                            except (socket.gaierror,ssl.SSLError,TimeoutError,ConnectionRefusedError,OSError):
-                                writer.writerows([(prog, ip, revers_lookup[ip], '-')])
-                                bar() 
+                            except (socket.gaierror, ssl.SSLError, TimeoutError, ConnectionRefusedError, OSError):
+                                writer.writerows(
+                                    [(prog, ip, revers_lookup[ip], '-')])
+                                bar()
                                 continue
         return revers_lookup
 
-
-    def reslove_func_ip(self,ip): #func to performe reverse ip lookup and dns history/ ssl alt names
-        ipv4 = re.compile('^(?:(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$')
+    # func to performe reverse ip lookup and dns history/ ssl alt names
+    def reslove_func_ip(self, ip):
+        ipv4 = re.compile(
+            '^(?:(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$')
         match_ip = ipv4.match(ip)
         try:
-            choice = int(input('Choose option:\n 1) Search for DNS history through Threat Crowd and VirusTotal APIs (recommended).\
+            choice = int(
+                input('Choose option:\n 1) Search for DNS history through Threat Crowd and VirusTotal APIs (recommended).\
             \n 2) Get SSL certificate alternative names (not recommended for suspecious connections as it connects to each IP).\n:'))
         except ValueError:
             sys.exit('Unknown option!')
-        with open('Resolved-'+ip.replace(':','')+".csv", 'w') as file_out_csv:
-            writer = csv.writer(file_out_csv)            
+        with open('Resolved-' + ip.replace(':', '') + ".csv", 'w') as file_out_csv:
+            writer = csv.writer(file_out_csv)
             if choice == 1:
-                writer.writerows([('Remote host', 'Domain name/IP', 'DNS history')]) 
+                writer.writerows(
+                    [('Remote host', 'Domain name/IP', 'DNS history')])
             elif choice == 2:
-                writer.writerows([('Remote host', 'Domain name/IP', 'SSL alternative names')])
+                writer.writerows(
+                    [('Remote host', 'Domain name/IP', 'SSL alternative names')])
             else:
                 sys.exit('Unknown option!')
             print('Resolving...')
             notFound = '-'
             if match_ip:
                 try:
-                    revers_lookup = (socket.gethostbyaddr(ip))            
+                    revers_lookup = (socket.gethostbyaddr(ip))
                 except (socket.herror, socket.gaierror, OSError):
                     revers_lookup = notFound
             else:
                 try:
-                    revers_lookup = (socket.gethostbyname(ip))            
+                    revers_lookup = (socket.gethostbyname(ip))
                 except (socket.herror, socket.gaierror, OSError):
-                    revers_lookup = notFound            
+                    revers_lookup = notFound
             if choice == 1:
-                threat_crowdAPI = requests.get("http://www.threatcrowd.org/searchApi/v2/ip/report/", {"ip": ip})
+                threat_crowdAPI = requests.get(
+                    "http://www.threatcrowd.org/searchApi/v2/ip/report/", {"ip": ip})
                 threat_crowdJSON = json.loads(threat_crowdAPI.text)
-                vt_api = requests.get(f"https://www.virustotal.com/vtapi/v2/ip-address/report?apikey={self.vt_api_key}&ip={ip}")
+                vt_api = requests.get(
+                    f"https://www.virustotal.com/vtapi/v2/ip-address/report?apikey={self.vt_api_key}&ip={ip}")
                 try:
                     vt_apiJSON = json.loads(vt_api.text)
-                    writer.writerows([(ip, revers_lookup, vt_apiJSON['resolutions'], threat_crowdJSON['resolutions'][::-1])])
-                except:
+                    writer.writerows([(ip,
+                                       revers_lookup,
+                                       vt_apiJSON['resolutions'],
+                                       threat_crowdJSON['resolutions'][::-1])])
+                except BaseException:
                     print("No or incorrect VT api key supplied")
                     try:
-                        writer.writerows([(ip, revers_lookup, threat_crowdJSON['resolutions'][::-1])])
-                        
+                        writer.writerows(
+                            [(ip, revers_lookup, threat_crowdJSON['resolutions'][::-1])])
+
                     except KeyError:
                         print("error")
                         writer.writerows([(ip, revers_lookup, '-')])
-                        
+
             elif choice == 2:
                 try:
                     x509 = reqs.OpenSSL.crypto.load_certificate(
@@ -171,13 +202,13 @@ class NstatResolver:
                         reqs.ssl.get_server_certificate((ip, 443)))
                     dns_alt_names = reqs.get_subj_alt_name(x509)
                     writer.writerows([(ip, revers_lookup, dns_alt_names)])
-                    
-                except (socket.gaierror,ssl.SSLError,TimeoutError,ConnectionRefusedError,OSError):
-                    writer.writerows([(ip, revers_lookup, '-')])                    
-        return revers_lookup        
+
+                except (socket.gaierror, ssl.SSLError, TimeoutError, ConnectionRefusedError, OSError):
+                    writer.writerows([(ip, revers_lookup, '-')])
+        return revers_lookup
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     try:
         n = NstatResolver()
     except IndexError:
